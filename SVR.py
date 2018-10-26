edaa58585ab2a9a63ff0b7a16a5b499b5e41a9e3
nukotsuka <nukotsuka@gmail.com>
2018-10-26 14:29:20 +0900

    Add SVR

---
+++ SVR.py
@@ -0,0 +1,147 @@
+from SVM import set_kernel
+import cvxopt
+import argparse
+from pylab import *
+import os
+
+
+def lagrange(X, Y, C, epsilon, kernel):
+    N = len(X)
+    tmp = np.zeros((N * 2, N * 2))
+    for i in range(N):
+        for j in range(N):
+            tmp[i, j] = kernel(X[i], X[j])
+    P = cvxopt.matrix(tmp)
+    tmp = np.zeros((N, N))
+    for i in range(N):
+        tmp[i, i] = -Y[i]
+    q = cvxopt.matrix(np.hstack((-np.squeeze(Y, 1), epsilon * np.ones(N))))
+    G = cvxopt.matrix(np.vstack((
+        np.hstack((np.diag([0.5] * N), np.diag([0.5] * N))),
+        np.hstack((np.diag([-0.5] * N), np.diag([0.5] * N))),
+        np.hstack((np.diag([-0.5] * N), np.diag([-0.5] * N))),
+        np.hstack((np.diag([0.5] * N), np.diag([-0.5] * N)))
+    )))
+    h = cvxopt.matrix(np.hstack((C * np.ones(N * 2), np.zeros(N * 2))))
+    A = cvxopt.matrix(np.hstack((np.ones(N), np.zeros(N))), (1, N * 2), 'd')
+    b = cvxopt.matrix(0.0)
+
+    cvxopt.solvers.options['abstol'] = 1e-5
+    cvxopt.solvers.options['reltol'] = 1e-10
+    cvxopt.solvers.options['show_progress'] = False
+    result = cvxopt.solvers.qp(P, q, G=G, h=h, A=A, b=b, kktsolver='ldl')
+    x = np.array(result['x']).reshape(N * 2)
+    A = (x[:N] + x[N:]) / 2
+    A_star = (x[N:] - x[:N]) / 2
+    # print(np.hstack((A, A_star)))
+    return A, A_star
+
+
+def classifier(X, Y, C, epsilon, kernel):
+    A, A_star = lagrange(X, Y, C, epsilon, kernel)
+
+    S = []
+    S_star = []
+
+    for i in range(len(A)):
+        if epsilon < A[i] < C - epsilon:
+            S.append(i)
+
+    for i in range(len(A_star)):
+        if epsilon < A_star[i] < C - epsilon:
+            S_star.append(i)
+
+    w = 0
+    for k in np.hstack((S, S_star)):
+        w += (A[k] - A_star[k]) * X[k]
+
+    sum = 0
+    for n in S:
+        tmp = 0
+        for k in range(len(A)):
+            tmp += (A[k] - A_star[k]) * kernel(X[n], X[k])
+        sum += -Y[n] + epsilon + tmp
+
+    for n in S_star:
+        tmp = 0
+        for k in range(len(A_star)):
+            tmp += (A[k] - A_star[k]) * kernel(X[n], X[k])
+        sum += -Y[n] - epsilon + tmp
+    theta = sum / len(np.hstack((S, S_star)))
+
+    print('w =', w)
+    print('θ =', theta)
+
+    def f(x):
+        sum = 0.0
+        for k in np.hstack((S, S_star)):
+            sum += (A[k] - A_star[k]) * kernel(X[k], x)
+        return sum - theta
+
+    return f, S
+
+
+def plot(args, data, f, S):
+    for n in S:
+        plt.plot(data[n,0], data[n,1], 'bo', markersize=10)
+
+    for d in data:
+        plt.plot(d[0], d[1], 'kx')
+
+
+    X1, X2 = meshgrid(linspace(0, 1.2, 300), linspace(0, 1.2, 300))
+    w, h = X1.shape
+    X1.resize(X1.size)
+    X2.resize(X2.size)
+    Z = array([f(array([x1, x2])) for (x1, x2) in zip(X1, X2)])
+    X1.resize((w, h))
+    X2.resize((w, h))
+    Z.resize((w, h))
+    contours = contour(X1, X2, Z, linewidths=1, origin='lower')
+    clabel(contours, inline=1, fontsize=10)
+
+    plt.xlim(0, 1.2)
+    plt.ylim(0, 1.2)
+
+    file = str(args.file).replace('sample_', '').replace('.txt', '')
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if not os.path.exists('./result'):
+        os.mkdir('./result')
+    plt.savefig('./result/' + args.kernel + '_' + file + date + '.png')
+    print(args.kernel + '_' + file + date + '.png is saved in result folder.')
+    print('please open:')
+    print('open result/' + args.kernel + '_' + file + date + '.png')
+
+
+def set_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("kernel", help='select from "no", "polynomial", "gauss"', type=str)
+    parser.add_argument("file", help='input data file name', type=str)
+    parser.add_argument("--d", type=float, help='for polynomial kernel', default=2)
+    parser.add_argument("--sigma", type=float,  help='for gauss kernel', default=np.sqrt(5))
+    parser.add_argument("--c", type=float,  help='slack variable for soft margin', default=1000)
+    parser.add_argument("--epsilon", type=float,  help='for epsilon insensitive function', default=0.1)
+    args = parser.parse_args()
+    return args
+
+
+def set_data(args):
+    data = np.loadtxt(args.file, delimiter=",")
+    D = len(data[0][:-1])  # dimension of data point
+    X = data[:, :D]
+    Y = data[:, D:]
+    return data, D, X, Y
+
+
+def main():
+    args = set_args()
+    kernel = set_kernel(args.kernel, args.d, args.sigma)
+    data, D, X, Y = set_data(args)
+    f, S = classifier(X, Y, args.c, args.epsilon, kernel)
+
+    if D == 2:
+        plot(args, data, f, S)
+
+
+if __name__ == '__main__':
+    main()
