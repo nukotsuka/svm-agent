97fe8db68731174f05ae367014323127ad5c230f
nukotsuka <nukotsuka@gmail.com>
2018-11-01 14:48:25 +0900

    Change to use epsilon list

--- svr_cross_validation.py
+++ svr_cross_validation.py
@@ -16,8 +16,8 @@ def set_args():
     parser.add_argument("--sigma_from", type=float, help='start point of sigma range for gauss kernel', default=0.5)
     parser.add_argument("--sigma_to", type=float, help='end point of sigma range for gauss kernel', default=5)
     parser.add_argument("--sigma_interval", type=float, help='interval of sigma range for gauss kernel', default=0.5)
-    parser.add_argument("--epsilon", type=float, help='for epsilon insensitive function', default=0.1)
-    parser.add_argument("--c_list", type=list,  help='slack variable list', default=[1, 10, 100, 1000])
+    parser.add_argument("-el", "--epsilon_list",  type=float, action="append", help='for epsilon insensitive function', default=[])
+    parser.add_argument("-cl", "--c_list", type=int, action="append",  help='slack variable list', default=[])
     args = parser.parse_args()
     return args
 
@@ -38,11 +38,11 @@ def cross_validation(args):
     N = args.n  # division number
     kernel_name = args.kernel
     file_name = args.file
-    epsilon = args.epsilon
 
     X, Y, D = divide_data(file_name, N)
 
     C_list = args.c_list
+    epsilon_list = args.epsilon_list
     d_range = np.arange(args.d_from, args.d_to + args.d_interval, args.d_interval)
     sigma_range = np.arange(args.sigma_from, args.sigma_to + args.sigma_interval, args.sigma_interval)
 
@@ -55,43 +55,39 @@ def cross_validation(args):
         kernel_param_range = sigma_range
         kernel_param_name = 'sigma'
 
-    kernel_param_points, C_points = np.meshgrid(kernel_param_range, C_list)
+    min_error = 100000000000
+    min_error_params = {}
+    for kernel_param in kernel_param_range:
+        for C in C_list:
+            for epsilon in epsilon_list:
+                kernel = set_kernel(kernel_name, d=kernel_param, sigma=kernel_param)
 
-    loss_matrix = np.zeros((len(C_list), len(kernel_param_range)))
-    min_loss = 100000000000
-    min_loss_params = {}
-    for i in range(len(kernel_param_range)):
-        for j in range(len(C_list)):
-            kernel_param = kernel_param_points[j][i]
-            C = C_points[j][i]
+                total_squares_error = 0
+                for n in range(N):
+                    X_ = X.copy()
+                    Y_ = Y.copy()
+                    X_test = X_.pop(n)
+                    Y_test = Y_.pop(n)
+                    X_train = np.vstack(X_)
+                    Y_train = np.vstack(Y_)
 
-            kernel = set_kernel(kernel_name, d=kernel_param, sigma=kernel_param)
+                    f, S = classifier(X_train, Y_train, C, epsilon, kernel)
 
-            total_squares_loss = 0
-            for n in range(N):
-                X_ = X.copy()
-                Y_ = Y.copy()
-                X_test = X_.pop(n)
-                Y_test = Y_.pop(n)
-                X_train = np.vstack(X_)
-                Y_train = np.vstack(Y_)
+                    squares_error = 0
+                    for i in range(len(X_test)):
+                        squares_error += (f(X_test[i]) - Y_test[i]) ** 2
+                    total_squares_error +=  squares_error / len(X_test)
 
-                f, S = classifier(X_train, Y_train, C, epsilon, kernel)
+                average_squares_error = total_squares_error / N
+                print('{0} = {1:4}, C = {2:4}, epsilon = {3:4}, average_squares_error = {4}'.format(kernel_param_name, kernel_param, C, epsilon, average_squares_error[0]))
+                if average_squares_error < min_error:
+                    min_error = average_squares_error
+                    min_error_params = {kernel_param_name: kernel_param, 'C': C, 'epsilon': epsilon}
 
-                squares_loss = 0
-                for i in range(len(X_test)):
-                    squares_loss += (f(X_test[i]) - Y_test[i]) ** 2
-                total_squares_loss +=  squares_loss / len(X_test)
-
-            average_squares_loss = total_squares_loss / N
-            print(kernel_param_name, '=', kernel_param, 'C =', C, 'average_squares_loss =', average_squares_loss)
-            loss_matrix[j][i] = average_squares_loss
-            if average_squares_loss < min_loss:
-                min_loss = average_squares_loss
-                min_loss_params = {kernel_param_name: kernel_param, 'C': C}
-
-    print('min loss =', min_loss)
-    print('min loss params =', min_loss_params)
+    print('file:', file_name)
+    print('kernel:', kernel_name)
+    print('min error =', min_error)
+    print('min error params =', min_error_params)
 
 
 def main():
